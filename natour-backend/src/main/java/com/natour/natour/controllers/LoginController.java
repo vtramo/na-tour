4c034f49f4addc927fe8ad0aa530cb289c21637f
Vincenzo Tramo <vv.tramo@gmail.com>
2022-07-30 16:17:37 +0200

    doc(login): add documentation to LoginController

--- natour-backend/src/main/java/com/natour/natour/controllers/LoginController.java
+++ natour-backend/src/main/java/com/natour/natour/controllers/LoginController.java
@@ -1,23 +1,34 @@
 package com.natour.natour.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.natour.natour.model.AuthenticationResponse;
 import com.natour.natour.model.Credentials;
 import com.natour.natour.services.authentication.AuthenticationService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@RequestMapping("/login/")
+@Tag(
+    name = "Login Controller", 
+    description = "This REST controller provide services to log in the NaTour application"
+)
 public class LoginController {
 
     @Autowired
     private AuthenticationService authenticationService;
 
-    @PostMapping(
-        path = "/login",
-        consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    @PostMapping(consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides a JWT Token for the supplied credentials")
     public AuthenticationResponse login(Credentials credentials) {
         try {
             return authenticationService.authenticate(credentials);
@@ -27,8 +38,10 @@ public class LoginController {
     }
 
     @PostMapping(
-        path = "/login/google",
+        path = "google",
         consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides a JWT Token for the supplied Google's authentication code")
     public AuthenticationResponse loginWithGoogle(String authenticationCode) {
         try {
             return authenticationService.authenticateWithGoogle(authenticationCode);
@@ -38,9 +51,11 @@ public class LoginController {
     }
 
     @PostMapping(
-        path = "/login/facebook",
+        path = "facebook",
         consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}
     )
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides a JWT Token for the supplied Facebook's access token")
     public AuthenticationResponse loginWithFacebook(String accessToken) {
         try {
             return authenticationService.authenticateWithFacebook(accessToken);
