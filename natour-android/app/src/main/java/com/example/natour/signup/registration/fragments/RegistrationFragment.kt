6f36e52cb7dfe77bb82a628281f4e8ae58d3997f
Vincenzo Tramo <vv.tramo@gmail.com>
2022-07-30 18:50:56 +0200

    feat(signup): add name, usrname and email controls
    Add first name, last name and email controls in RegistrationFragment.kt.
    First and last name max 16 characters (same for username).

--- natour-android/app/src/main/java/com/example/natour/signup/registration/fragments/RegistrationFragment.kt
+++ natour-android/app/src/main/java/com/example/natour/signup/registration/fragments/RegistrationFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.natour.R
 import com.example.natour.databinding.FragmentRegistrationBinding
 
 
@@ -16,23 +15,78 @@ class RegistrationFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true);
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-       _binding = FragmentRegistrationBinding.inflate(inflater,container,false);
-        val view = binding.root
-        return view
+    ): View {
+        _binding = FragmentRegistrationBinding.inflate(inflater,container,false);
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupFirstnameListener()
+        setupLastnameListener()
+        setupEmailListener()
+
+    }
+
+    private fun setupFirstnameListener() {
+        binding.firstNameTextInputEditText.setOnFocusChangeListener { _, isFocused ->
+            if (!isFocused) validateFirstname()
+        }
+    }
+
+    private fun validateFirstname() {
+        val firstName = binding.firstNameTextInputEditText.text.toString()
+        if (!Regex.NAME_REGEX.matches(firstName)) {
+            binding.firstNameTextInputLayout.isErrorEnabled = true
+            binding.firstNameTextInputLayout.error = "Incorrect first name"
+        } else {
+            binding.firstNameTextInputLayout.isErrorEnabled = false
+            binding.firstNameTextInputLayout.error = null
+        }
+    }
+
+    private fun setupLastnameListener() {
+        binding.lastNameTextInputEditText.setOnFocusChangeListener { _, isFocused ->
+            if (!isFocused) validateLastname()
+        }
+    }
+
+    private fun validateLastname() {
+        val lastName = binding.lastNameTextInputEditText.text.toString()
+        if (!Regex.NAME_REGEX.matches(lastName)) {
+            binding.lastNameTextInputLayout.isErrorEnabled = true
+            binding.lastNameTextInputLayout.error = "Incorrect last name"
+        } else {
+            binding.lastNameTextInputLayout.isErrorEnabled = false
+            binding.lastNameTextInputLayout.error = null
+        }
+    }
+
+    private fun setupEmailListener() {
+        binding.emailTextInputEditText.setOnFocusChangeListener { _, isFocused ->
+            if (!isFocused) validateEmail()
+        }
+    }
+
+    private fun validateEmail() {
+        val email = binding.emailTextInputEditText.text.toString()
+        if (!Regex.EMAIL_REGEX.matches(email)) {
+            binding.emailTextInputLayout.isErrorEnabled = true
+            binding.emailTextInputLayout.error = "Incorrect email"
+        } else {
+            binding.emailTextInputLayout.isErrorEnabled = false
+            binding.emailTextInputLayout.error = null
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
