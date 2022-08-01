3e61fb7b7af7655377c0eac070c53233c33da17e
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-01 15:53:03 +0200

    feat(user): add properties/methods to the MainUser object
    Add properties (user's details and token) to the MainUser
    object and methods to set/clear these properties.

--- natour-android/app/src/main/java/com/example/natour/user/MainUser.kt
+++ natour-android/app/src/main/java/com/example/natour/user/MainUser.kt
@@ -1,18 +1,53 @@
 package com.example.natour.user
 
+import com.example.natour.model.AuthenticationResponse
+import java.lang.IllegalArgumentException
+
 object MainUser {
-    private var _id = Int.MIN_VALUE
-    var id
-        get() = _id
-        set(value) { _id = value }
+    private data class Details(
+        var id:         Long   = Long.MIN_VALUE,
+        var username:   String = "",
+        var firstName:  String = "",
+        var lastName:   String = "",
+        var email:      String = ""
+    )
 
-    private var _username = ""
-    var username
-        get() = _username
-        set(value) { _username = value }
+    private data class Token(
+        var accessToken:    String = "",
+        var refreshToken:   String = ""
+    )
 
-    private var _email = ""
-    var email
-        get() = _email
-        set(value) { _email = value }
+    private var details = Details()
+    val id          get() = details.id
+    val username    get() = details.username
+    val firstName   get() = details.firstName
+    val lastName    get() = details.lastName
+    val email       get() = details.email
+
+    private var token = Token()
+    val accessToken     get() = token.accessToken
+    val refreshToken    get() = token.refreshToken
+
+    fun set(authentication: AuthenticationResponse) {
+        if (!authentication.authenticated)
+            throw IllegalArgumentException("Authentication object must be authenticated!")
+
+        with(details) {
+            id          = authentication.id!!
+            username    = authentication.username!!
+            firstName   = authentication.firstName!!
+            lastName    = authentication.lastName!!
+            email       = authentication.email!!
+        }
+
+        with(token) {
+            accessToken     = authentication.accessToken!!
+            refreshToken    = authentication.refreshToken!!
+        }
+    }
+
+    fun clear() {
+        details = Details()
+        token   = Token()
+    }
 }
