773dceb3b6851fbc44d19288a4234ed3eb917918
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-02 09:38:09 +0200

    feat(data): add UserRemoteDataSource

---
+++ natour-android/app/src/main/java/com/example/natour/data/sources/UserRemoteDataSource.kt
@@ -0,0 +1,17 @@
+package com.example.natour.data.sources
+
+import com.example.natour.data.model.AuthenticationResponse
+
+interface UserRemoteDataSource {
+    suspend fun login(username: String, password: String) : AuthenticationResponse
+    suspend fun loginWithGoogle(authenticationCode: String) : AuthenticationResponse
+    suspend fun loginWithFacebook(accessToken: String) : AuthenticationResponse
+
+    suspend fun register(
+        firstName: String,
+        lastName: String,
+        username: String,
+        email: String,
+        password: String
+    ): Boolean
+}
---
+++ natour-android/app/src/main/java/com/example/natour/data/sources/impl/UserRemoteDataSourceImpl.kt
@@ -0,0 +1,40 @@
+package com.example.natour.data.sources.impl
+
+import com.example.natour.data.model.AuthenticationResponse
+import com.example.natour.data.sources.UserRemoteDataSource
+import com.example.natour.data.sources.network.services.login.LoginApiService
+import com.example.natour.data.sources.network.services.registration.RegistrationApiService
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+class UserRemoteDataSourceImpl(
+    private val loginApiService: LoginApiService,
+    private val registerApiService: RegistrationApiService,
+    private val ioDispatcher: CoroutineDispatcher
+) : UserRemoteDataSource {
+
+    override suspend fun login(username: String, password: String): AuthenticationResponse =
+        withContext(ioDispatcher) {
+            loginApiService.login(username, password)
+        }
+
+    override suspend fun loginWithGoogle(authenticationCode: String): AuthenticationResponse =
+        withContext(ioDispatcher) {
+            loginApiService.loginWithGoogle(authenticationCode)
+        }
+
+    override suspend fun loginWithFacebook(accessToken: String): AuthenticationResponse =
+        withContext(ioDispatcher) {
+            loginApiService.loginWithFacebook(accessToken)
+        }
+
+    override suspend fun register(
+        firstName: String,
+        lastName: String,
+        username: String,
+        email: String,
+        password: String
+    ): Boolean = withContext(ioDispatcher) {
+        registerApiService.register(firstName, lastName, username, email, password)
+    }
+}
