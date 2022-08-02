5cb3355cb6bc20ed7cf1d5b7bc7782d9b5774b40
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-02 09:57:03 +0200

    refactor: add digger annotations to activity

--- natour-android/app/src/main/java/com/example/natour/MainActivity.kt
+++ natour-android/app/src/main/java/com/example/natour/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.appcompat.content.res.AppCompatResources
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
