d0e85862c0c8eccdf169f4f822b711baaaf90192
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-03 13:00:16 +0200

    refactor(domain): refactor Login and Registratin use cases

--- natour-android/app/src/main/java/com/example/natour/domain/LogInUserUseCase.kt
+++ natour-android/app/src/main/java/com/example/natour/domain/LogInUserUseCase.kt
@@ -2,34 +2,35 @@ package com.example.natour.domain
 
 import androidx.lifecycle.LiveData
 import com.example.natour.data.model.AuthenticationResponse
-import com.example.natour.data.repositories.UserRepository
-import com.example.natour.user.MainUser
+import com.example.natour.data.repositories.AuthenticatedUserRepository
+import com.example.natour.data.repositories.MainUserRepository
+import com.example.natour.data.model.AuthenticationResult
 import kotlinx.coroutines.coroutineScope
 
 class LogInUserUseCase(
-    private val userRepository: UserRepository,
-    private val mainUser: MainUser
+    private val authenticatedUserRepository: AuthenticatedUserRepository,
+    private val mainUserRepository: MainUserRepository
 ) {
 
-    val isAuthenticated: LiveData<Boolean> = userRepository.isAuthenticated
+    val isAuthenticated: LiveData<AuthenticationResult> = authenticatedUserRepository.isAuthenticated
 
     suspend fun login(username: String, password: String) = coroutineScope {
-        val authenticationResponse = userRepository.login(username, password)
-        setMainUsersIfAuthenticated(authenticationResponse)
+        val authenticationResponse = authenticatedUserRepository.login(username, password)
+        saveMainUserIfAuthenticated(authenticationResponse)
     }
 
     suspend fun loginWithGoogle(authenticationCode: String) = coroutineScope {
-        val authenticationResponse = userRepository.loginWithGoogle(authenticationCode)
-        setMainUsersIfAuthenticated(authenticationResponse)
+        val authenticationResponse = authenticatedUserRepository.loginWithGoogle(authenticationCode)
+        saveMainUserIfAuthenticated(authenticationResponse)
     }
 
     suspend fun loginWithFacebook(accessToken: String) = coroutineScope {
-        val authenticationResponse = userRepository.loginWithFacebook(accessToken)
-        setMainUsersIfAuthenticated(authenticationResponse)
+        val authenticationResponse = authenticatedUserRepository.loginWithFacebook(accessToken)
+        saveMainUserIfAuthenticated(authenticationResponse)
     }
 
-    private fun setMainUsersIfAuthenticated(authentication: AuthenticationResponse) {
+    private fun saveMainUserIfAuthenticated(authentication: AuthenticationResponse) {
         val isLogged = authentication.authenticated
-        if (isLogged) mainUser.set(authentication)
+        if (isLogged) mainUserRepository.save(authentication)
     }
 }
--- natour-android/app/src/main/java/com/example/natour/domain/RegistrationUserUseCase.kt
+++ natour-android/app/src/main/java/com/example/natour/domain/RegistrationUserUseCase.kt
@@ -2,26 +2,26 @@ package com.example.natour.domain
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.natour.data.repositories.UserRepository
+import com.example.natour.data.repositories.AuthenticatedUserRepository
+import com.example.natour.data.model.RegistrationResult
+import kotlinx.coroutines.coroutineScope
 
-class RegistrationUserUseCase(private val userRepository: UserRepository) {
+class RegistrationUserUseCase(private val authenticatedUserRepository: AuthenticatedUserRepository) {
 
-    val hasBeenRegistered: LiveData<Boolean> = userRepository.hasBeenRegistered
+    val hasBeenRegistered: LiveData<RegistrationResult> = authenticatedUserRepository.hasBeenRegistered
 
     private val _userExists = MutableLiveData<Boolean>()
     val userExists: LiveData<Boolean> get() = _userExists
 
     suspend fun register(
-        firstName: String,
-        lastName: String,
-        username: String,
-        password: String,
-        email: String
-    ) {
-        if (userRepository.existsByUsername(username)) {
+        firstName: String, lastName: String,
+        username: String, password: String, email: String
+    ) = coroutineScope {
+        if (authenticatedUserRepository.existsByUsername(username)) {
             _userExists.value = true
+            _userExists.value = false // Reset
         } else {
-            userRepository.register(firstName, lastName, username, email, password)
+            authenticatedUserRepository.register(firstName, lastName, username, email, password)
         }
     }
 }
