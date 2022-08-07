63c456cf7ea4026d474d9611f5a0145fc4f8db19
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-07 12:09:55 +0200

    refactor(route): move map to a different fragment

--- natour-android/app/src/main/java/com/example/natour/ui/route/RouteCreationFragment.kt
+++ natour-android/app/src/main/java/com/example/natour/ui/route/RouteCreationFragment.kt
@@ -3,7 +3,6 @@ package com.example.natour.ui.route
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -13,6 +12,8 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
 import com.example.natour.R
 import com.example.natour.databinding.FragmentRouteCreationBinding
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -20,26 +21,32 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.*
 
-class RouteCreationFragment : Fragment(), OnMapReadyCallback,
-    GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
-
-    private lateinit var mMap: GoogleMap
+class RouteCreationFragment : Fragment() {
 
     private var _binding: FragmentRouteCreationBinding? = null
     private val binding get() = _binding!!
+
+    private val mRouteCreationViewModel: RouteCreationViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_route_creation, container, false)
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_route_creation,
+            container,
+            false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.routeCreationFragment = this
+
         setupRouteDifficultyDropDownList()
-        setupGoogleMap()
     }
 
     private fun setupRouteDifficultyDropDownList() {
@@ -51,7 +58,13 @@ class RouteCreationFragment : Fragment(), OnMapReadyCallback,
         )
     }
 
-    private fun setupGoogleMap() {
+    fun goToRouteTypeCreationFragment() {
+        val action = RouteCreationFragmentDirections
+            .actionRouteCreationFragmentToRouteTypeCreationFragment()
+        view?.findNavController()?.navigate(action)
+    }
+
+/*    private fun startGoogleMap() {
         val mapFragment = childFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragmentWrapper
 
@@ -66,36 +79,29 @@ class RouteCreationFragment : Fragment(), OnMapReadyCallback,
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         enableMyLocation()
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
-        val polyline = mMap.addPolyline(
-            PolylineOptions()
-                .add(sydney)
-                .color(Color.RED)
-        )
-
-        val pattern = listOf(
-            Dot(), Gap(20F), Dash(30F), Gap(20F)
-        )
-        polyline.pattern = pattern
-
-        mMap.setOnMapClickListener {
-            polyline.points = polyline.points + it
+        mMap.setOnMapClickListener { point ->
+            setStartPositionMarker(point.latitude, point.longitude)
         }
+    }
+
+    private fun setStartPositionMarker(lat: Double, lng: Double) {
+        mStartPositionMarker?.remove()
+        mStartPositionMarker = mMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(lat, lng))
+                .title("Starting point of the route")
+                .draggable(true)
+        )
     }
 
     @SuppressLint("MissingPermission")
     private fun enableMyLocation() {
-        if (locationPermissionsAreAlreadyGranted()) {
+        if (locationPermissionsAreGranted())
             mMap.isMyLocationEnabled = true
-            return
-        }
     }
 
-    private fun locationPermissionsAreAlreadyGranted() =
+    private fun locationPermissionsAreGranted() =
         ContextCompat.checkSelfPermission(
             requireContext(),
             Manifest.permission.ACCESS_FINE_LOCATION
@@ -104,11 +110,48 @@ class RouteCreationFragment : Fragment(), OnMapReadyCallback,
             Manifest.permission.ACCESS_COARSE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
 
-    override fun onMyLocationButtonClick(): Boolean {
-        TODO("Not yet implemented")
+    fun onGoButtonClick() {
+        val latString = binding.latitudeTextInputEditText.text.toString()
+        val lngString = binding.longitudeTextInputEditText.text.toString()
+
+        if (latString.isBlank() || lngString.isBlank()) {
+            setErrorLatLngTextInputLayout()
+            return
+        }
+
+        val lat = latString.toDouble()
+        val lng = latString.toDouble()
+        val latLngCoordinatesAreCorrect = checkLatitudeValue(lat) && checkLongitudeValue(lng)
+
+        if (!latLngCoordinatesAreCorrect) {
+            setErrorLatLngTextInputLayout()
+            return
+        }
+
+        setStartPositionMarker(lat, lng)
+        mMap.animateCamera(
+            CameraUpdateFactory.newCameraPosition(
+                CameraPosition.Builder()
+                    .target(LatLng(lat, lng))
+                    .zoom(10F)
+                    .build()
+            )
+        )
+
+        binding.latitudeTextInputLayout.isErrorEnabled = false
+        binding.longitudeTextInputLayout.isErrorEnabled = false
     }
 
-    override fun onMyLocationClick(p0: Location) {
-        TODO("Not yet implemented")
+    private fun setErrorLatLngTextInputLayout() {
+        binding.latitudeTextInputLayout.isErrorEnabled = true
+        binding.latitudeTextInputLayout.error = "Invalid value"
+        binding.longitudeTextInputLayout.isErrorEnabled = true
+        binding.longitudeTextInputLayout.error = "Invalid value"
     }
+
+    private fun checkLatitudeValue(lat: Double) : Boolean  = lat in -90.0..90.0
+    private fun checkLongitudeValue(lng: Double): Boolean  = lng in -180.0..180.
+
+    override fun onMyLocationButtonClick(): Boolean = true
+    override fun onMyLocationClick(p0: Location) { }*/
 }
