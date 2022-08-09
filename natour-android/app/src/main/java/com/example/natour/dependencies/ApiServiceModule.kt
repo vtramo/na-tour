0da3577054a2def64493da9193fc7d03ddab2a73
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-09 22:26:07 +0200

    feat: add dependencies to ApiServiceModule

--- natour-android/app/src/main/java/com/example/natour/dependencies/ApiServiceModule.kt
+++ natour-android/app/src/main/java/com/example/natour/dependencies/ApiServiceModule.kt
@@ -3,8 +3,10 @@ package com.example.natour.dependencies
 import com.example.natour.data.sources.network.LoginApiService
 import com.example.natour.data.sources.network.services.LoginService
 import com.example.natour.data.sources.network.RegistrationApiService
+import com.example.natour.data.sources.network.TrailApiService
 import com.example.natour.data.sources.network.UserApiService
 import com.example.natour.data.sources.network.services.RegistrationService
+import com.example.natour.data.sources.network.services.TrailService
 import com.example.natour.data.sources.network.services.UserService
 import dagger.Module
 import dagger.Provides
@@ -28,4 +30,8 @@ object ApiServiceModule {
     @Provides
     @Singleton
     fun provideUserApiService() : UserApiService = UserService.retrofitService
+
+    @Provides
+    @Singleton
+    fun provideTrailApiService() : TrailApiService = TrailService.retrofitService
 }
