de93ba5a4184cedc87436dad945545c7d9a0fd1f
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-09 22:19:44 +0200

    feat: save the route at the end of its creation

--- natour-android/app/src/main/java/com/example/natour/ui/trail/fragments/TrailDisplayCreationFragment.kt
+++ natour-android/app/src/main/java/com/example/natour/ui/trail/fragments/TrailDisplayCreationFragment.kt
@@ -1,5 +1,6 @@
 package com.example.natour.ui.trail.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,19 +10,20 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import com.example.natour.R
 import com.example.natour.databinding.FragmentTrailDisplayCreationBinding
-import com.example.natour.ui.trail.TrailCreationViewModel
+import com.example.natour.ui.trail.TrailStartCreationViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import com.google.android.material.snackbar.Snackbar
 
 class TrailDisplayCreationFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentTrailDisplayCreationBinding? = null
     private val binding get() = _binding!!
 
-    private val mTrailCreationViewModel: TrailCreationViewModel by activityViewModels()
+    private val mTrailStartCreationViewModel: TrailStartCreationViewModel by activityViewModels()
     private lateinit var mListOfRoutePoints: List<LatLng>
 
     private lateinit var mMap: GoogleMap
@@ -29,9 +31,9 @@ class TrailDisplayCreationFragment : Fragment(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        mListOfRoutePoints = mTrailCreationViewModel
+        mListOfRoutePoints = mTrailStartCreationViewModel
                     .listOfRoutePoints
-                    .map { LatLng(it.first, it.second) }
+                    .map { LatLng(it.latitude, it.longitude) }
     }
 
     override fun onCreateView(
@@ -50,6 +52,7 @@ class TrailDisplayCreationFragment : Fragment(), OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
 
         binding.lifecycleOwner = viewLifecycleOwner
+        binding.trailDisplayCreationFragment = this
 
         startGoogleMap()
     }
@@ -98,6 +101,35 @@ class TrailDisplayCreationFragment : Fragment(), OnMapReadyCallback {
     }
 
     fun onConfirmButtonClick() {
+        binding.confirmButton.isClickable = false
+        with(mTrailStartCreationViewModel) {
+            hasBeenCreated.observe(viewLifecycleOwner) { hasBeenCreated ->
+                if (hasBeenCreated) {
+                    showTrailSuccessfullyCreatedSnackbar()
+                    // TODO: GO HOME
+                } else {
+                    showFailTrailCreationAlertDialog()
+                    binding.confirmButton.isClickable = true
+                    resetLiveData()
+                }
+            }
+            saveTrail()
+        }
+    }
 
+    private fun showTrailSuccessfullyCreatedSnackbar() {
+        Snackbar.make(
+            requireView(),
+            "Trail successfully created",
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun showFailTrailCreationAlertDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Error")
+            .setMessage("A problem occurred in the creation of the trail")
+            .setPositiveButton("Okay") { _, _ -> }
+            .show()
     }
 }
