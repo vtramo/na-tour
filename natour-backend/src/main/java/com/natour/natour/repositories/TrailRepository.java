6f41b0bf9af6e1119f75044081af4d427c5f7cc6
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-09 22:21:10 +0200

    feat: add TrailRepository

--- natour-backend/src/main/java/com/natour/natour/repositories/TrailRepository.java
+++ natour-backend/src/main/java/com/natour/natour/repositories/TrailRepository.java
@@ -1,5 +1,8 @@
 package com.natour.natour.repositories;
 
+import java.math.BigDecimal;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
