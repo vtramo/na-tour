435def33eca883e5c9bb9d9c468d7bba7401775c
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-10 11:33:04 +0200

    feat(entity): add TrailReview entity

---
+++ natour-backend/src/main/java/com/natour/natour/model/entity/TrailReview.java
@@ -0,0 +1,37 @@
+package com.natour.natour.model.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.natour.natour.model.TrailReviewStars;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "NT_TRAIL_REVIEWS")
+public class TrailReview {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade=CascadeType.ALL)
+    private ApplicationUser owner;
+
+    @ManyToOne(cascade=CascadeType.ALL)
+    private Trail trail;
+
+    private TrailReviewStars stars;
+
+    private String description;
+}
