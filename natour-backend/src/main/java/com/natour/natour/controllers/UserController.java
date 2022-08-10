4fcac5fc10f58542fc211f6ed162c1598ee79545
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-10 20:29:26 +0200

    feat: add favorite trails endpoint

--- natour-backend/src/main/java/com/natour/natour/controllers/UserController.java
+++ natour-backend/src/main/java/com/natour/natour/controllers/UserController.java
@@ -3,8 +3,11 @@ package com.natour.natour.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +30,21 @@ public class UserController {
     @Operation(summary = "Returns true if the provided username already exists")
     public boolean existsByUsername(String username) {
         return applicationUserService.existsByUsername(username);
+    }
+
+    @PutMapping( 
+        path = "trail/favorite/{userId}",
+        consumes = {MediaType.ALL_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Add the provided trail to the favorite trail list of " + 
+                                "the provided user")
+    public boolean addFavoriteTrail(
+        @PathVariable("userId") long userId,
+        @RequestParam long trailId
+    ) {
+        System.out.println(userId + " " + trailId);
+        return true;
     }
 }
