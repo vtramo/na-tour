549460e7047a20d3ccaabb12d1ee89f606297c74
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-11 17:15:20 +0200

    feat: add TrailPagingAndSortingRepository

--- natour-backend/src/main/java/com/natour/natour/repositories/TrailRepository.java
+++ natour-backend/src/main/java/com/natour/natour/repositories/TrailRepository.java
@@ -1,11 +1,11 @@
 package com.natour.natour.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.natour.natour.model.entity.Trail;
 
 @Repository
-public interface TrailRepository extends CrudRepository<Trail, Long> {
+public interface TrailRepository extends PagingAndSortingRepository<Trail, Long> {
     
 }
