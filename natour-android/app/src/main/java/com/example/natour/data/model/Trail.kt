59037b88c27366010eebeaf463ce159023bf2cde
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-12 14:31:49 +0200

    feat: add Trail data class

--- natour-android/app/src/main/java/com/example/natour/data/model/Trail.kt
+++ natour-android/app/src/main/java/com/example/natour/data/model/Trail.kt
@@ -1,8 +1,17 @@
 package com.example.natour.data.model
 
+import android.graphics.drawable.Drawable
+
 data class Trail(
+    val idTrail: Long,
+    val owner: UserDetails,
     val name: String,
-    val owner: String,
-    val image: Int,
-    val country: String
+    val description: String,
+    val difficulty: TrailDifficulty,
+    val duration: Duration,
+    val image: Drawable,
+    val routePoints: List<RoutePoint>,
+    val photos: List<TrailPhoto>,
+    val reviews: List<TrailReview>,
+    val stars: Stars
 )
