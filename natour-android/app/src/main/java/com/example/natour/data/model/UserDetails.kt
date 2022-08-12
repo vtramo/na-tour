76dc7956c17cd2a9e8bf4db1c7f0eea003326b0b
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-12 14:26:01 +0200

    feat: add UserDetails data class

---
+++ natour-android/app/src/main/java/com/example/natour/data/model/UserDetails.kt
@@ -0,0 +1,9 @@
+package com.example.natour.data.model
+
+data class UserDetails (
+    val id: Long,
+    val username: String,
+    val firstName: String,
+    val lastName: String,
+    val email: String
+)
