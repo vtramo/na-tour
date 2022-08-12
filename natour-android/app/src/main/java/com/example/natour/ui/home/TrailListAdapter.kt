bd8188dbcc98eaafcf8889a30bfc5c2a7e66c335
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-12 14:29:11 +0200

    feat: add TrailListAdapter

---
+++ natour-android/app/src/main/java/com/example/natour/ui/home/TrailListAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.natour.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.natour.data.model.Trail
+import com.example.natour.databinding.CardViewBinding
+
+class TrailListAdapter(
+    private val trailCardClickListener: (Trail) -> Unit
+): ListAdapter<Trail, TrailListAdapter.TrailCardViewHolder>(TrailDiffCallback) {
+
+    companion object {
+        private val TrailDiffCallback = object : DiffUtil.ItemCallback<Trail>() {
+            override fun areItemsTheSame(oldItem: Trail, newItem: Trail): Boolean =
+                oldItem.idTrail == newItem.idTrail
+
+            override fun areContentsTheSame(oldItem: Trail, newItem: Trail): Boolean =
+                oldItem == newItem
+        }
+    }
+
+    inner class TrailCardViewHolder(
+        private val binding: CardViewBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bindTrail(trail: Trail) = with(binding) {
+            trailImage.setImageDrawable(trail.image)
+            cardTrailName.text = trail.name
+            cardOwnerName.text = trail.owner.username
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TrailCardViewHolder {
+        val binding = CardViewBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        val viewHolder = TrailCardViewHolder(binding)
+
+        binding.trailImage.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            trailCardClickListener(getItem(position))
+        }
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(trailCardViewHolder: TrailCardViewHolder, position: Int) {
+        trailCardViewHolder.bindTrail(getItem(position))
+    }
+}
