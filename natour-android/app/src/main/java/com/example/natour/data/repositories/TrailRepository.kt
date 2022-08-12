313a171c6c48989b59600d1515e0df96c0b47205
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-12 14:33:44 +0200

    feat: add fetch trails to Trail Repository

--- natour-android/app/src/main/java/com/example/natour/data/repositories/TrailRepository.kt
+++ natour-android/app/src/main/java/com/example/natour/data/repositories/TrailRepository.kt
@@ -3,7 +3,9 @@ package com.example.natour.data.repositories
 import android.graphics.drawable.Drawable
 import com.example.natour.data.model.Duration
 import com.example.natour.data.model.RoutePoint
+import com.example.natour.data.model.Trail
 import com.example.natour.data.model.TrailDifficulty
+import kotlinx.coroutines.flow.Flow
 
 
 interface TrailRepository {
@@ -16,4 +18,6 @@ interface TrailRepository {
         routePoints: List<RoutePoint>,
         image: Drawable
     ): Boolean
+
+    suspend fun load(page: Int): Flow<List<Trail>>
 }
