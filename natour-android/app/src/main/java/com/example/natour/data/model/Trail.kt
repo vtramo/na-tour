141178975e9b5f3920421c814007b586ab8dac76
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-12 15:22:02 +0200

    feat: add stars images

--- natour-android/app/src/main/java/com/example/natour/data/model/Trail.kt
+++ natour-android/app/src/main/java/com/example/natour/data/model/Trail.kt
@@ -1,6 +1,8 @@
 package com.example.natour.data.model
 
 import android.graphics.drawable.Drawable
+import com.example.natour.MainActivity
+import com.example.natour.R
 
 data class Trail(
     val idTrail: Long,
@@ -14,4 +16,16 @@ data class Trail(
     val photos: List<TrailPhoto>,
     val reviews: List<TrailReview>,
     val stars: Stars
-)
+) {
+    fun getStarsImage(): Drawable =
+        with(MainActivity) {
+            when(stars) {
+                Stars.ZERO  -> getDrawable(R.drawable.zero_stars_image)!!
+                Stars.ONE   -> getDrawable(R.drawable.one_stars_image)!!
+                Stars.TWO   -> getDrawable(R.drawable.two_stars_image)!!
+                Stars.THREE -> getDrawable(R.drawable.three_stars_image)!!
+                Stars.FOUR  -> getDrawable(R.drawable.four_stars_image)!!
+                Stars.FIVE  -> getDrawable(R.drawable.five_stars_image)!!
+            }
+        }
+}
