8b0184d601ee27a03e754dfb0205af247e98aefc
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-23 15:27:55 +0200

    feat: add check connection to login

---
+++ natour-android/app/src/main/java/com/example/natour/exceptions/ErrorMessages.kt
@@ -0,0 +1,6 @@
+package com.example.natour.exceptions
+
+object ErrorMessages {
+    const val CONNECTION_ERROR = "Connection error"
+    const val CONNECTION_ERROR_SERVER = "Something went wrong in contacting the server. Please check your connection."
+}
