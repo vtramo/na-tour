02aa5c2acc3d09e388f486b0c5c50b2919ba810c
Vincenzo Tramo <vv.tramo@gmail.com>
2022-08-25 12:27:15 +0200

    refactor(ui): refactor login ui

--- natour-android/app/src/main/java/com/example/natour/ui/signin/LoginFragment.kt
+++ natour-android/app/src/main/java/com/example/natour/ui/signin/LoginFragment.kt
@@ -55,7 +55,7 @@ class LoginFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
 
         Glide.with(requireContext())
-            .load(MainActivity.getDrawable(R.drawable.forest_gif))
+            .load(MainActivity.getDrawable(R.drawable.login_gif))
             .into(binding.gifNatour)
     }
 
